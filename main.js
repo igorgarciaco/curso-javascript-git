422c7d36ae8206ffe6875b908fa3d17f8929cc17
Igor Garcia <igorgarciaco@gmail.com>
2018-05-19 13:27:20 -0300

    Fisrt Commit

---
+++ main.js
@@ -0,0 +1,37 @@
+// nome = "Igor Garcia";
+// isDev = false;
+
+// if (isDev == true) {
+//   console.log('é dev')
+// } else {
+//   console.log('não é')
+// }
+
+// tipoDev = 'expert';
+
+// switch (tipoDev) {
+//   case 'expert':
+//     print('nousa')
+//     break;
+
+
+// }
+
+// numero1 = 2
+// numero2 = 5
+
+// total = numero1 + numero2;
+
+// function soma(primeiroNum, segundoNum) {
+//   total = primeiroNum + segundoNum;
+//   console.log(total)
+
+// }
+
+// soma(numero1, numero2);
+
+let soma = function (parametro1, parametro2){
+  console.log(parametro1 + parametro2);
+}
+
+soma(1,5);
